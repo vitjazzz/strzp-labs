3b71b4fa2a53ef064b9a3e2d6d4837283130e007
pisarenko <oa.pisarenko@gmail.com>
2018-12-09 18:07:14 +0200

    not sure its working

--- src/main/java/com/study/strzp/telegram/bot/entity/Currency.java
+++ src/main/java/com/study/strzp/telegram/bot/entity/Currency.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class Currency {
 
-    private String name;
+    private String ccy;
 
     private String buy;
 
--- src/main/java/com/study/strzp/telegram/bot/service/impl/CurrencyServiceImpl.java
+++ src/main/java/com/study/strzp/telegram/bot/service/impl/CurrencyServiceImpl.java
@@ -1,26 +1,50 @@
 package com.study.strzp.telegram.bot.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.strzp.telegram.bot.entity.Currency;
 import com.study.strzp.telegram.bot.service.CommandService;
 import com.study.strzp.telegram.bot.util.MessageFormatter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Qualifier("currencyService")
 public class CurrencyServiceImpl implements CommandService {
 
+    private final RestTemplate restTemplate;
+
+    public CurrencyServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public SendMessage handle(Update update) {
 
-        String bucksOfficeSell = "";
-        String bucksOfficeBuy = "";
+        String bucksOfficeSell;
+        String bucksOfficeBuy;
         String euroOfficeSell = "";
         String euroOfficeBuy = "";
         String bucksOnlineSell = "";
         String bucksOnlineBuy = "";
         String euroOnlineSell = "";
         String euroOnlineBuy = "";
+
+        List<Currency> urlResponse = getUrlResponse("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5");
+
+        bucksOfficeBuy = urlResponse.get(0).getBuy();
+        bucksOfficeSell = urlResponse.get(0).getSale();
 
 
         SendMessage sendMessage = new SendMessage();
@@ -49,6 +73,14 @@ public class CurrencyServiceImpl implements CommandService {
                 "Покупка:  <b>" + euroOnlineBuy + " грн.</b>");
 
         return sendMessage;
+    }
+
+
+    private List<Currency> getUrlResponse(String url) {
+
+
+        return Collections.singletonList(restTemplate.getForObject(url, Currency.class));
+
     }
 
 }
