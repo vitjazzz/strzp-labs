9bb895072ea14e8c94cf9425724b66cb7d18ebe5
vitjazzz <viktor.zikratyi@gmail.com>
2018-12-09 15:41:10 +0200

    Added services

---
+++ src/main/java/com/study/strzp/telegram/bot/service/StartService.java
@@ -0,0 +1,11 @@
+package com.study.strzp.telegram.bot.service;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+@Service
+public class StartService {
+    public SendMessage handle(){
+        return null;
+    }
+}
