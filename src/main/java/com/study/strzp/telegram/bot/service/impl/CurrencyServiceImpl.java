974ca5e59ca3c96fc606eb0e8b9592b3c305d9da
pisarenko <oa.pisarenko@gmail.com>
2018-12-09 16:19:35 +0200

    Service for currencies request

---
+++ src/main/java/com/study/strzp/telegram/bot/service/impl/CurrencyServiceImpl.java
@@ -0,0 +1,49 @@
+package com.study.strzp.telegram.bot.service.impl;
+
+import com.study.strzp.telegram.bot.service.CommandService;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class CurrencyServiceImpl implements CommandService {
+
+    public SendMessage handle(Update update) {
+
+        String bucksOfficeSell = "";
+        String bucksOfficeBuy = "";
+        String euroOfficeSell = "";
+        String euroOfficeBuy = "";
+        String bucksOnlineSell = "";
+        String bucksOnlineBuy = "";
+        String euroOnlineSell = "";
+        String euroOnlineBuy = "";
+
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(update.getCallbackQuery().getFrom().getId().toString());
+        sendMessage.setText("Наличный курс ПриватБанка (в отделениях):\n" +
+                " \n" +
+                "<b>\uD83D\uDCB5USD (Доллар США)</b>\n" +
+                "Продажа:  <b>" + bucksOfficeSell + " грн.</b>\n" +
+                "Покупка:  <b>" + bucksOfficeBuy + " грн.</b>\n" +
+                " \n" +
+                "<b>\uD83D\uDCB6EUR (Евро)</b>\n" +
+                "Продажа:  <b>" + euroOfficeSell + " грн.</b>\n" +
+                "Покупка:  <b>" + euroOfficeBuy + " грн.</b>\n" +
+                " \n" +
+                "Безналичный курс ПриватБанка (конвертация по картам, Приват24, пополнение вкладов):\n" +
+                " \n" +
+                "<b>\uD83D\uDCB5USD (Доллар США)</b>\n" +
+                "Продажа:  <b>" + bucksOnlineSell + " грн.</b>\n" +
+                "Покупка:  <b>" + bucksOnlineBuy + " грн.</b>\n" +
+                " \n" +
+                "<b>\uD83D\uDCB6EUR (Евро)</b>\n" +
+                "Продажа:  <b>" + euroOnlineSell + " грн.</b>\n" +
+                "Покупка:  <b>" + euroOnlineBuy + " грн.</b>");
+
+        return sendMessage;
+    }
+
+}
