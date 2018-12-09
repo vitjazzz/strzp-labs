f29a87f8662e83fca66d0950d7a3083f60f33328
vitjazzz <viktor.zikratyi@gmail.com>
2018-12-09 15:30:00 +0200

    Removed unused imports

--- src/main/java/com/study/strzp/telegram/bot/MyTestBot.java
+++ src/main/java/com/study/strzp/telegram/bot/MyTestBot.java
@@ -1,19 +1,10 @@
 package com.study.strzp.telegram.bot;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-
-import javax.annotation.PostConstruct;
-import java.io.Serializable;
-import java.util.List;
 
 public class MyTestBot extends TelegramLongPollingBot {
 
