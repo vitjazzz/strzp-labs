6891bd143ac329d7ad8fb6285dbb17ed7ba48a2a
pisarenko <oa.pisarenko@gmail.com>
2018-12-09 16:44:59 +0200

    added entity fields

--- src/main/java/com/study/strzp/telegram/bot/entity/Currency.java
+++ src/main/java/com/study/strzp/telegram/bot/entity/Currency.java
@@ -1,4 +1,15 @@
 package com.study.strzp.telegram.bot.entity;
 
+
+import lombok.Data;
+
+@Data
 public class Currency {
+
+    private String name;
+
+    private String buy;
+
+    private String sale;
+
 }
