6ca2b15727c1a588c8a525073b13b5c908a15790
vitjazzz <viktor.zikratyi@gmail.com>
2018-12-09 18:15:27 +0200

    Added get request to yandex.

--- src/main/java/com/study/strzp/telegram/bot/MyTestBot.java
+++ src/main/java/com/study/strzp/telegram/bot/MyTestBot.java
@@ -8,11 +8,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Location;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MyTestBot extends TelegramLongPollingBot {
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(100);
 
     @Autowired
     @Qualifier("startServiceImpl")
@@ -21,6 +27,10 @@ public class MyTestBot extends TelegramLongPollingBot {
     @Autowired
     @Qualifier("currencyService")
     CommandService currencyService;
+
+    @Autowired
+    @Qualifier("atmService")
+    CommandService atmService;
 
     @Autowired
     DefaultMessageHandler defaultMessageHandler;
@@ -33,33 +43,42 @@ public class MyTestBot extends TelegramLongPollingBot {
     String token;
 
     public void onUpdateReceived(Update update) {
-        CallbackQuery callback = update.getCallbackQuery();
-        Message message = update.getMessage();
-        SendMessage replyMessage = null;
-        if(callback != null){
-            switch (callback.getData()) {
-                case "/currency":
-                    replyMessage = currencyService.handle(update);
-                    break;
-                default:
-                    replyMessage = defaultMessageHandler.handle(callback.getFrom().getId().toString());
-                    break;
+        executorService.execute(() -> {
+            CallbackQuery callback = update.getCallbackQuery();
+            Message message = update.getMessage();
+            SendMessage replyMessage = null;
+            if(callback != null){
+                switch (callback.getData()) {
+                    case "/currency":
+                        replyMessage = currencyService.handle(update);
+                        break;
+                    case "/atm":
+                        replyMessage = new SendMessage().setChatId(message.getChatId()).setText("Пожалуйста, пришлите мне свою локацию.");
+                    default:
+                        replyMessage = defaultMessageHandler.handle(callback.getFrom().getId().toString());
+                        break;
+                }
+            } else if (message != null){
+                if(message.getLocation() != null){
+                    replyMessage = atmService.handle(update);
+                } else if(message.getText() != null) {
+                    switch (message.getText()) {
+                        case "/start":
+                            replyMessage = startService.handle(update);
+                            break;
+                        default:
+                            replyMessage = defaultMessageHandler.handle(message.getChatId().toString());
+                            break;
+                    }
+                }
+            } else
+                return;
+            try {
+                execute(replyMessage);
+            } catch (TelegramApiException e) {
+                System.out.println("Exception: " + e.toString());
             }
-        } else if (message != null){
-            switch (message.getText()) {
-                case "/start":
-                    replyMessage = startService.handle(update);
-                    break;
-                default:
-                    replyMessage = defaultMessageHandler.handle(message.getChatId().toString());
-                    break;
-            }
-        }
-        try {
-            execute(replyMessage);
-        } catch (TelegramApiException e) {
-            System.out.println("Exception: " + e.toString());
-        }
+        });
     }
 
 
